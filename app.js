69b912b3739a6f795fb56add50444d08addc965c
zach <62062622+zach12904@users.noreply.github.com>
2022-02-16 09:23:09 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      title: 'The Final Empire',
+      author: 'Brandon Sanderson',
+      age: 45
+    }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks
+    }
+  }
+})
+
+app.mount('#app')
